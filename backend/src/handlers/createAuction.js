ecb18b2cf2098f0922e8d54f99de7d22503fa8fd
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-10 18:38:06 +0530

    feat: Create Auction in DynamoDB

--- backend/src/handlers/createAuction.js
+++ backend/src/handlers/createAuction.js
@@ -1,11 +1,28 @@
+import * as AWS from "aws-sdk";
+import * as uuid from "uuid";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const auctionTable = process.env.AUCTION_TABLE;
+
 async function createAuction(event, context) {
     const body = JSON.parse(event.body);
     const timestamp = new Date().toISOString();
+    const auctionId = uuid.v4();
+
     const auction = {
+        auctionId: auctionId,
         title: body.title,
         status: "OPEN",
         createdAt: timestamp,
     };
+
+    await docClient
+        .put({
+            TableName: auctionTable,
+            Item: auction,
+        })
+        .promise(); // By default, AWS APIs use callbacks.. So, to use async/await, we have to put .promise()
+
     return {
         statusCode: 201, // Resource created
         body: JSON.stringify({ auction }),
