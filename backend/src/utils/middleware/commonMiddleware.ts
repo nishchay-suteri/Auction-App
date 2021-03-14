0f08470c4d4a487f5f810500468eb5336330f154
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-14 18:18:50 +0530

    feat: Enable cors headers with Authentication

--- backend/src/utils/middleware/commonMiddleware.ts
+++ backend/src/utils/middleware/commonMiddleware.ts
@@ -1,7 +1,12 @@
 import middy from "@middy/core";
 import httpEventNormalizer from "@middy/http-event-normalizer";
 import httpErrorHandler from "@middy/http-error-handler";
+import cors from "@middy/http-cors";
 
 export default (handler: any) => {
-    return middy(handler).use([httpEventNormalizer(), httpErrorHandler()]);
+    return middy(handler).use([
+        httpEventNormalizer(),
+        httpErrorHandler(),
+        cors({ credentials: true }),
+    ]);
 };
