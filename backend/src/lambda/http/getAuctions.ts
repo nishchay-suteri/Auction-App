65361429b13ccefe6683be47b13a521dbd632798
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-14 11:32:32 +0530

    feat: validate query parameters
    serverless-reqvalidator-plugin does not support query paramter VALUE validation support.
    So, had to put the logic in lambda function

--- backend/src/lambda/http/getAuctions.ts
+++ backend/src/lambda/http/getAuctions.ts
@@ -6,23 +6,37 @@ import { createLogger } from "../../utils/logger";
 import commonMiddleware from "../../utils/middleware/commonMiddleware";
 
 const logger = createLogger("lambda-http-getAuctions");
+import { BadRequest } from "http-errors";
 
 async function getAuctions(
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
     logger.info(`Processing Event: ${event}`);
-    const status = event.queryStringParameters.status;
+    let status: string = event.queryStringParameters.status;
 
-    try {
-        const auctions = await getAuctionItems(status);
-        logger.info("Success");
-        return {
-            statusCode: 200,
-            body: JSON.stringify(auctions),
-        };
-    } catch (err) {
-        logger.error(`Failure: ${err}`);
-        throw err;
+    logger.info(`Passed Qeury parameter - status is ${status}`);
+
+    if (!status) {
+        // if status is undefined
+        status = "OPEN";
+        logger.info(`status changed to OPEN`);
+    }
+
+    if (status === "OPEN" || status === "CLOSED") {
+        try {
+            const auctions = await getAuctionItems(status);
+            logger.info("Success");
+            return {
+                statusCode: 200,
+                body: JSON.stringify(auctions),
+            };
+        } catch (err) {
+            logger.error(`Failure: ${err}`);
+            throw err;
+        }
+    } else {
+        logger.error(`Failure: Unsupported status passed - ${status}`);
+        throw new BadRequest(`Provide a valid status`);
     }
 }
 
