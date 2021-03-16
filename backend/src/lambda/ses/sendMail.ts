da88b1b9401f38589fcd19fb4bb9523eb3fb2902
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-16 13:33:34 +0530

    feat: Setup SQS for Email Service
    SQS Resources added to Cloud Formation Stack.
    Setup SES to Receive Mail from SQS and Process it.

--- backend/src/lambda/ses/sendMail.ts
+++ backend/src/lambda/ses/sendMail.ts
@@ -1,5 +1,5 @@
 import * as AWS from "aws-sdk";
-
+import { SQSEvent, SQSRecord } from "aws-lambda";
 import { createLogger } from "../../utils/logger";
 
 // SES - Amazon Simple Email Service
@@ -8,16 +8,23 @@ const ses = new AWS.SES({ region: "us-east-1" });
 
 const logger = createLogger("lambda-ses-sendMail");
 
-async function sendMail(event): Promise<AWS.SES.SendEmailResponse> {
-    // TODO: Type of event
+async function sendMail(event: SQSEvent): Promise<AWS.SES.SendEmailResponse> {
     logger.info(`Processing Event: ${event}`);
+
+    // Handle requests coming from SQS
+    const record: SQSRecord = event.Records[0]; // Single and only response we are going to process - batchSize is 1
+    logger.info(`Record received from SQS: ${JSON.stringify(record)}`);
+
+    const email = JSON.parse(record.body);
+
+    const { subject, body, recipient } = email;
 
     const params: AWS.SES.SendEmailRequest = {
         Source: "nishchay.dev@gmail.com", // The Email Address Verified in SES Console
-        Destination: { ToAddresses: ["nishchay271095@gmail.com"] },
+        Destination: { ToAddresses: [recipient] },
         Message: {
-            Body: { Text: { Data: "Hello There from Nishchay" } },
-            Subject: { Data: "Test Mail" },
+            Body: { Text: { Data: body } },
+            Subject: { Data: subject },
         },
     };
     try {
