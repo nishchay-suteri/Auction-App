35af87b5f01f8846239e0467ebe80779d8314bc5
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-16 09:00:12 +0530

    feat: Send Mail From SES service
    Currently using hardcoded Email Address and Content for the Mail.

--- backend/src/lambda/ses/sendMail.ts
+++ backend/src/lambda/ses/sendMail.ts
@@ -1,13 +1,36 @@
+import * as AWS from "aws-sdk";
+
 import { createLogger } from "../../utils/logger";
 
 // SES - Amazon Simple Email Service
 
+const ses = new AWS.SES({ region: "us-east-1" });
+
 const logger = createLogger("lambda-ses-sendMail");
 
-async function sendMail(event) {
-    logger.info(`Processing Event: ${event}`);
+async function sendMail(event): Promise<AWS.SES.SendEmailResponse> {
     // TODO: Type of event
-    return event;
+    logger.info(`Processing Event: ${event}`);
+
+    const params: AWS.SES.SendEmailRequest = {
+        Source: "nishchay.dev@gmail.com", // The Email Address Verified in SES Console
+        Destination: { ToAddresses: ["nishchay271095@gmail.com"] },
+        Message: {
+            Body: { Text: { Data: "Hello There from Nishchay" } },
+            Subject: { Data: "Test Mail" },
+        },
+    };
+    try {
+        const result: AWS.SES.SendEmailResponse = await ses
+            .sendEmail(params)
+            .promise();
+        logger.info(
+            `Send Email - Success : Result - ${JSON.stringify(result)}`
+        );
+        return result;
+    } catch (err) {
+        logger.error(`Send Email - Failure : Error - ${err}`);
+    }
 }
 
 export const handler = sendMail;
